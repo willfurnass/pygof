6649ec1fdeb38f46af326fe0a4c33347a06d7ecd
Will Furnass <will@thearete.co.uk>
2014-04-17 14:27:45 +0100

    Added absolute_import support

--- pygof/__init__.py
+++ pygof/__init__.py
@@ -1,3 +1,4 @@
 """Goodness of fit metrics for comparing (primarily time-series) observation and prediction vectors."""
+from __future__ import absolute_import
 
-from metrics import nash_sutcliffe, r_squared, rmsd, avg_pc_err, integ_sq_errors, integ_weighted_sq_errors
+from .metrics import nash_sutcliffe, r_squared, rmsd, avg_pc_err, integ_sq_errors, integ_weighted_sq_errors
